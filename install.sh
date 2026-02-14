5d4532412f9783e9365033505b0927992e700e31
Jonis Kiesbye <j.kiesbye@tum.de>
2026-02-14 22:47:34 +0100

    Update libgirepository from version 1.0 to 2.0

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Install required software
 sudo apt update
 sudo apt install git python3 python3-venv python3-pip openjdk-17-jdk xdot
-sudo apt install libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-3.0 libgraphviz-dev
+sudo apt install libgirepository-2.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-3.0 libgraphviz-dev
 python3 -m venv python3-venv
 source python3-venv/bin/activate
 pip install wheel
